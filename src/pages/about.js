6d0ee1a2d531da3892a07dc0756026c0ef27ad57
muhammadgani <muhammmadgani@wimoku.com>
2021-01-10 10:37:36 +0530

    adds about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,10 +2,22 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="page">
-      <div className="container">
-        <div className="row">
-          <h3>This is the About page</h3>
+    <div className="container">
+      <div className="about-container">
+        <div className="about-title">LEAGUM BACCALAURES</div>
+        <div className="about-small-title">“Nurturing Careers in Law !”</div>
+        <div className="about-description">
+          LEAGUM BACCALAURES LAW CENTER is the No.1 CLAT- Law entrance coaching
+          Centre , you can partner with for CLAT and LLB entrance coaching,
+          which has been ensuring outstanding, consistent record of excellent
+          results for the last 10 years. It is the FIRST in Malabar being
+          started in 2008. It is the BEST, because of its outstanding results in
+          all previous years. It is DIFFERENT, being the only such centre
+          managed by Law Professionals. WE a team of LAW PROFESSIONALS AND
+          SUBJECT EXPERTS are committed to offering Class room / online coaching
+          for Legal reasoning, English comprehension, grammar and verbal
+          ability, Current affairs and General knowledge, Logical reasoning &
+          Numerical ability
         </div>
       </div>
     </div>
